050f36e23e3fecf0391c652d3989d4abb8e13531
kinus-is-coding <stasianery123@gmail.com>
2024-08-30 16:23:57 +0700

    settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -150,3 +151,4 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS=True
 CORS_ALLOWS_CREDENTIALS=True
+CORS_ALLOW_HEADERS = ['*']
